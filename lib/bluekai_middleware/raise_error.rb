292319c4bdfe273890ed28ae16b4d0c954a7792c
Jason Petersen <jpetersen@bluekai.com>
2012-05-15 11:36:34 -0500

    Add StatusCodeFix class for badly-behaved APIs
    Certain APIs always return a 200, instead putting the "real" semantic
    status in a field of the response. This middleware assumes any JSON/XML
    parsing has already occurred, after which time it looks for the status
    in the response body (assuming the actual status was a 200). If the
    "real" status is not 'QUERY_SUCCESS', the HTTP status is changed to a
    400 to allow later middlewares to do the right thing. The approach is
    similar to the MimeTypeFix class in the faraday_middleware gem.
    
    cr: @Nymbil, @jzhang-bk

--- lib/bluekai_middleware/raise_error.rb
+++ lib/bluekai_middleware/raise_error.rb
@@ -5,6 +5,22 @@ require 'faraday'
 module BlueKaiMiddleware
   # Raises a server or client error depending upon the response status
   class RaiseError < Faraday::Response::Middleware
+
+    class StatusCodeFix < Faraday::Response::Middleware
+      SUCCESS_STATUS = 'QUERY_SUCCESS'.freeze
+
+      # Some APIs annoyingly return a 200 status code even though some-
+      # thing is wrong with the query. This attempts to detect these
+      # responses and changes their status code to a 400.
+      def on_complete(env)
+        status, body = *env.values_at(:status, :body)
+
+        if status == 200 && body.is_a?(Hash)
+          env.status = 400 if body['status'] != SUCCESS_STATUS
+        end
+      end
+    end
+
     # Inspects the status of the environment and raises an error when
     # necessary
     def on_complete(env)
