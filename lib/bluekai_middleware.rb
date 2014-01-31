369ac8eb3854c00e467d879f3de6c9aea065de6b
Kristine Robison <kris@bluekai.com>
2014-01-30 16:34:29 -0800

    Fixes per a first pass of BlueKai's `rubocop` settings
    Current `.rubocop.yml` file:
    
    ```
    # Multi-line method chaining should be done with leading dots.
    DotPosition:
      Style: trailing
      SupportedStyles:
        - leading
        - trailing
    
    MethodLength:
      CountComments: false  # count full line comments?
      Max: 15
    
    SpaceInsideHashLiteralBraces:
      EnforcedStyle: no_space
      EnforcedStyleForEmptyBraces: no_space
      SupportedStyles:
        - space
        - no_space
    
    Semicolon:
      # Allow ; to separate several expressions on the same line.
      AllowAsExpressionSeparator: true
    ```

--- lib/bluekai_middleware.rb
+++ lib/bluekai_middleware.rb
@@ -1,7 +1,10 @@
+# encoding: UTF-8
+
 require 'bluekai_middleware/version'
 require 'rack/utils'
 
-# This module contains cross-cutting code useful when interacting with any number of BlueKai services.
+# This module contains cross-cutting code useful when interacting with any
+# number of BlueKai services.
 module BlueKaiMiddleware
   autoload :Authenticate,  'bluekai_middleware/authenticate'
   autoload :LogSubscriber, 'bluekai_middleware/log_subscriber'
@@ -18,9 +21,9 @@ module BlueKaiMiddleware
     attr_reader :status, :body, :url
 
     # Creates a new HTTPError instance.
-    # @param [Fixnum] status the status of the response that resulted in this error
+    # @param [Fixnum] status the status of the response that threw this error
     # @param [String] body the body of the response that resulted in this error
-    def initialize(status = nil, body = nil, url=nil)
+    def initialize(status = nil, body = nil, url = nil)
       super(Rack::Utils::HTTP_STATUS_CODES[status])
       @status, @body, @url = status, body, url
     end
